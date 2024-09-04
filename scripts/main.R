3d033f5ac852d40b3df717749faec6d856668601
rusty-ccv <120149732+rusty-ccv@users.noreply.github.com>
2024-09-04 12:01:21 +1000

    Added header info.

---
+++ scripts/main.R
@@ -0,0 +1,13 @@
+# Stat2003 Assessment 2 R script 
+# For Data collection and Quantitative Analysis (STAT2003-2004-T4)
+# by Duncan Sage
+
+
+rm(list=ls())  # Remove any variables from memory.
+
+getwd()
+# setwd()
+
+#################################
+# Package dependencies
+################################
